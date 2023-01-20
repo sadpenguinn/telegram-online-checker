31a9b6ae0864ae81bae7f42ced1d031ce6cdbe72
Vladislav Badrudin <badrudinvladislav@gmail.com>
2023-01-20 18:54:23 +0300

    initial

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM python:3.9-slim as compiler
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /app/
+
+RUN python -m venv /opt/venv
+ENV PATH="/opt/venv/bin:$PATH"
+
+COPY ./requirements.txt /app/requirements.txt
+RUN pip install -Ur requirements.txt
+
+FROM python:3.9-slim as runner
+WORKDIR /app/
+COPY --from=compiler /opt/venv /opt/venv
+ENV PATH="/opt/venv/bin:$PATH"
+
+
+COPY telegram-online-checker.py /app/
+
+RUN python3 /app/telegram-online-checker.py
---
+++ telegram-online-checker.py
@@ -0,0 +1,59 @@
+import asyncio
+import datetime
+import logging
+import time
+import os
+
+from pyrogram import Client, enums
+import telebot
+
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+
+class TimeDefinition:
+    SECONDS = 1
+    MINUTES = 60
+    HOURS = 60 * 60
+
+
+def sleep(timeout_seconds):
+    logging.info('Sleep for {}'.format(str(datetime.timedelta(seconds=timeout_seconds))))
+    time.sleep(timeout_seconds)
+
+
+async def main():
+    client_name = 'telegram'
+    api_id = os.environ['TELEGRAM_API_ID']
+    api_hash = os.environ['TELEGRAM_API_HASH']
+    bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+    target_user = os.environ['TELEGRAM_TARGET_USER']
+    target_chat = os.environ['TELEGRAM_TARGET_CHAT']
+
+    bot = telebot.TeleBot(bot_token, parse_mode=None)
+
+    async with Client(client_name, api_id, api_hash) as telegram:
+        while True:
+            user = await telegram.get_users(target_user)
+
+            logging.info(user.last_online_date)
+            logging.info(user.status)
+
+            if user.status == enums.UserStatus.ONLINE:
+                bot.send_message(target_chat, '{} проснулась 🤤'.format(target_user))
+                logging.info('Send notification')
+                sleep(5 * TimeDefinition.HOURS)
+            else:
+                hour_now = datetime.datetime.now().hour
+                hour_last_online = user.last_online_date.hour
+                if hour_now < hour_last_online and hour_now < 5:
+                    sleep((5 - hour_now) * TimeDefinition.HOURS)
+                elif hour_now - hour_last_online < 5:
+                    sleep((5 - (hour_now - hour_last_online)) * TimeDefinition.HOURS)
+                else:
+                    sleep(3 * TimeDefinition.MINUTES)
+
+asyncio.run(main())
